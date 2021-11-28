93a90e600bb5dc27e6222b0409b232314650a7d5
Fernando Resendiz <resendizbautista801@gmail.com>
2021-11-27 19:51:27 -0600

    Basic template and config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,20 @@
 import React, { useEffect, useState } from "react";
-import getData from "../utils/getData";
+import Navbar from "./Navbar";
+import PlantInfo from "./PlantInfo";
 
 const App = () => {
-    const [count, setCount] = useState('0 Respuestas del Backend [La Rasp]');
+    const [active, setActive] = useState(1);
 
-    useEffect(()=>{
-        const fetchCount = async () => {
-            const data = await getData();
-            setCount(data);
-        };
-        fetchCount();
-    },[]);
-
-    const handleClick = async () => {
-        const data = await getData();
-        setCount(data);
+    const changePlant = (plant_num) => {
+        setActive(plant_num);
     }
-    return (
-        <div className="container">
-            <button className="button" onClick={handleClick}>Nueva Petición</button>
-            <h1>Respuestas del Backend: {count}</h1>
+
+    return(
+        <div>
+            <Navbar activePlant={active} setActivePlant={changePlant}></Navbar>
+            <PlantInfo activePlant={active} className={`Planta${active}`}></PlantInfo>
         </div>
-    );
+    )
 };
 
 export default App;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,11 +1,18 @@
 const API = 'http://cr7god.hopto.org';
 
-const getData = async () => {
+const getData = async (plant) => {
   const apiURL = API;
   try {
-    const response = await fetch(apiURL);
-    const data = await response.json();
-    return data.text;
+    // const response = await fetch(apiURL);
+    // const data = await response.json();
+    const data = {
+      id: plant,
+      temperature: 22,
+      light: 1,
+      humidity: 1,
+      watering: 1
+    }
+    return data;
   } catch (error) {
     console.log('Fetch Error', error);
   };
